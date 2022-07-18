143303f9f5c7c9459042b436fa04d7c5ea90c535
Bobby Trejo <81659806+bobbytrejo@users.noreply.github.com>
2022-07-17 18:51:15 -0700

    Completed Day 16 Project

--- drink-water/script.js
+++ drink-water/script.js
@@ -3,6 +3,8 @@ const liters = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
+updateBigCup()
+
 smallCups.forEach((cup, idx) => {
   cup.addEventListener('click', () => highlightCups(idx))
 })
@@ -19,4 +21,29 @@ function highlightCups(idx) {
         cup.classList.remove('full')
       }
   })
+
+  updateBigCup()
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.cup-small.full').length
+
+  const totalCups = smallCups.length
+
+  if(fullCups === 0) {
+    percentage.style.visibility = 'hidden'
+    percentage.style.height = 0
+  } else {
+    percentage.style.visibility = 'visible'
+    percentage.style.height = `${fullCups / totalCups * 330}px`
+    percentage.innerText = `${fullCups / totalCups * 100}%`
+  }
+
+  if(fullCups === totalCups) {
+    remained.style.visibility = 'hidden'
+    remained.style.height = 0
+  } else {
+    remained.style.visibility = 'visible'
+    liters.innerText = `${2 - (250 * fullCups / 1000)}L`
+  }
 }
